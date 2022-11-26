d26950ef3d0a421207e3c4d09eaa3a2567807abd
Kshitij <kshitijbhate@hotmail.com>
2022-11-26 15:43:17 +0530

    Added Navbar and Styles Components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from "react";
-
-import { footer, blog, possibility, features, whatGPT3, header } from './containers';
-import { CTA, brand, Navbar } from './components';
+import './App.css';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
       <div className="gradient__bg">
         <Navbar />
-
-
+        <Header />
 
       </div>
+
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+
+
+
 
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import logo from '../../assets/logo.svg'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+
+
+const Menu = () => {
+    <>
+        <p><a href="#home">Home</a></p>
+        <p><a href="#wgtp3">what is GPT3</a></p>
+        <p><a href="#possibilities">Open AI</a></p>
+        <p><a href="#features">Case Studies</a></p>
+        <p><a href="#blog">Library</a></p>
+    </>
+}
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
-        <div>navbar</div>
+        <div className='gpt3__navbar'>
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="LOGO" />
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#wgtp3">what is GPT3</a></p>
+                    <p><a href="#possibilities">Open AI</a></p>
+                    <p><a href="#features">Case Studies</a></p>
+                    <p><a href="#blog">Library</a></p>
+                </div>
+            </div>
+            <div className="gpt3__navbar-sign">
+                <p>sign in</p>
+                <button type='button'>Sign up</button>
+            </div>
+            <div className="gpt3__navbar-menu">
+                {toggleMenu
+                    ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} /> /// the "?" represents that if the condition is true, and the ":" represents if the condition is false
+                    : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />}
+                {toggleMenu && (
+                    <div className='gpt3__navbar-menu-container scale-up-center'>
+                        <div className='gpt3__navbar-menu_container-links'>
+                            <p><a href="#home">Home</a></p>
+                            <p><a href="#wgtp3">what is GPT3</a></p>
+                            <p><a href="#possibilities">Open AI</a></p>
+                            <p><a href="#features">Case Studies</a></p>
+                            <p><a href="#blog">Library</a></p>
+                            {/* Links Container End */}
+                        </div>
+                    </div>
+                )
+                }
+            </div>
+        </div>
     )
 }
 
