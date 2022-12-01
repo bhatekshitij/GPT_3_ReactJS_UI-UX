2e935b5dad7d50109ae092ea32f3e85b4ca14d0d
Kshitij <kshitijbhate@hotmail.com>
2022-12-01 14:21:19 +0530

    Wrote final media queries for case studies section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,7 +3,7 @@ import { Feature } from '../../components';
 import './whatGPT3.css';
 const WhatGPT3 = () => {
     return (
-        <div className='gpt3__whatgpt3 section__padding' id='wgpt3'>
+        <div className='gpt3__whatgpt3 section__padding' id='wgtp3'>
             <div className='gpt3__whatgpt3-feature' id='wgpt3'>
                 <Feature title='What is GPT 3' text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.' />
             </div>
